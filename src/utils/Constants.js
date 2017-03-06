d4f68e80ae9aa9b8504a0f0c27f214659c5f6776
Mindaugas Jačionis <mindaugasjacionis@Mindaugass-MacBook-Pro.local>
2017-03-07 00:43:28 +0200

    Adds proper logos and build settings for Android&iOS

--- src/utils/Constants.js
+++ src/utils/Constants.js
@@ -6,35 +6,35 @@ export const { width, height } = Dimensions.get('window');
 
 export const DATA = {
   e95: [
-    { id: 1, name: 'neste oil', logo: 'https://upload.wikimedia.org/wikipedia/en/thumb/8/83/Orlen_logo.svg/640px-Orlen_logo.svg.png', price: 1.02 },
-    { id: 2, name: 'neste oil', logo: 'https://upload.wikimedia.org/wikipedia/en/thumb/8/83/Orlen_logo.svg/640px-Orlen_logo.svg.png', price: 1.07 },
-    { id: 3, name: 'neste oil', logo: 'https://upload.wikimedia.org/wikipedia/en/thumb/8/83/Orlen_logo.svg/640px-Orlen_logo.svg.png', price: 1.05 },
-    { id: 4, name: 'neste oil', logo: 'https://upload.wikimedia.org/wikipedia/en/thumb/8/83/Orlen_logo.svg/640px-Orlen_logo.svg.png', price: 1.02 },
-    { id: 5, name: 'neste oil', logo: 'https://upload.wikimedia.org/wikipedia/en/thumb/8/83/Orlen_logo.svg/640px-Orlen_logo.svg.png', price: 1.09 },
-    { id: 6, name: 'neste oil', logo: 'https://upload.wikimedia.org/wikipedia/en/thumb/8/83/Orlen_logo.svg/640px-Orlen_logo.svg.png', price: 1.02 }
+    { id: 1, name: 'neste oil', logo: 'https://mindaugas-jacionis.github.io/degalu-kainos/img/orlen.png', price: 1.02 },
+    { id: 2, name: 'neste oil', logo: 'https://mindaugas-jacionis.github.io/degalu-kainos/img/lukoil.jpg', price: 1.07 },
+    { id: 3, name: 'neste oil', logo: 'https://mindaugas-jacionis.github.io/degalu-kainos/img/emsi.png', price: 1.05 },
+    { id: 4, name: 'neste oil', logo: 'https://mindaugas-jacionis.github.io/degalu-kainos/img/statoil.png', price: 1.02 },
+    { id: 5, name: 'neste oil', logo: 'https://mindaugas-jacionis.github.io/degalu-kainos/img/neste.png', price: 1.09 },
+    { id: 6, name: 'neste oil', logo: 'https://mindaugas-jacionis.github.io/degalu-kainos/img/Balticpetr.jpg', price: 1.02 }
   ],
   e98: [
-    { id: 1, name: 'neste oil', logo: 'https://upload.wikimedia.org/wikipedia/en/thumb/8/83/Orlen_logo.svg/640px-Orlen_logo.svg.png', price: 1.20 },
-    { id: 2, name: 'neste oil', logo: 'https://upload.wikimedia.org/wikipedia/en/thumb/8/83/Orlen_logo.svg/640px-Orlen_logo.svg.png', price: 1.22 },
-    { id: 3, name: 'neste oil', logo: 'https://upload.wikimedia.org/wikipedia/en/thumb/8/83/Orlen_logo.svg/640px-Orlen_logo.svg.png', price: 1.50 },
-    { id: 4, name: 'neste oil', logo: 'https://upload.wikimedia.org/wikipedia/en/thumb/8/83/Orlen_logo.svg/640px-Orlen_logo.svg.png', price: 1.23 },
-    { id: 5, name: 'neste oil', logo: 'https://upload.wikimedia.org/wikipedia/en/thumb/8/83/Orlen_logo.svg/640px-Orlen_logo.svg.png', price: 1.27 },
-    { id: 6, name: 'neste oil', logo: 'https://upload.wikimedia.org/wikipedia/en/thumb/8/83/Orlen_logo.svg/640px-Orlen_logo.svg.png', price: 1.20 }
+    { id: 1, name: 'neste oil', logo: 'https://mindaugas-jacionis.github.io/degalu-kainos/img/orlen.png', price: 1.20 },
+    { id: 2, name: 'neste oil', logo: 'https://mindaugas-jacionis.github.io/degalu-kainos/img/lukoil.jpg', price: 1.22 },
+    { id: 3, name: 'neste oil', logo: 'https://mindaugas-jacionis.github.io/degalu-kainos/img/emsi.png', price: 1.50 },
+    { id: 4, name: 'neste oil', logo: 'https://mindaugas-jacionis.github.io/degalu-kainos/img/statoil.png', price: 1.23 },
+    { id: 5, name: 'neste oil', logo: 'https://mindaugas-jacionis.github.io/degalu-kainos/img/neste.png', price: 1.27 },
+    { id: 6, name: 'neste oil', logo: 'https://mindaugas-jacionis.github.io/degalu-kainos/img/Balticpetr.jpg', price: 1.20 }
   ],
   dizelinas: [
-    { id: 1, name: 'neste oil', logo: 'https://upload.wikimedia.org/wikipedia/en/thumb/8/83/Orlen_logo.svg/640px-Orlen_logo.svg.png', price: 1.02 },
-    { id: 2, name: 'neste oil', logo: 'https://upload.wikimedia.org/wikipedia/en/thumb/8/83/Orlen_logo.svg/640px-Orlen_logo.svg.png', price: 1.00 },
-    { id: 3, name: 'neste oil', logo: 'https://upload.wikimedia.org/wikipedia/en/thumb/8/83/Orlen_logo.svg/640px-Orlen_logo.svg.png', price: 1.02 },
-    { id: 4, name: 'neste oil', logo: 'https://upload.wikimedia.org/wikipedia/en/thumb/8/83/Orlen_logo.svg/640px-Orlen_logo.svg.png', price: 1.08 },
-    { id: 5, name: 'neste oil', logo: 'https://upload.wikimedia.org/wikipedia/en/thumb/8/83/Orlen_logo.svg/640px-Orlen_logo.svg.png', price: 1.02 },
-    { id: 6, name: 'neste oil', logo: 'https://upload.wikimedia.org/wikipedia/en/thumb/8/83/Orlen_logo.svg/640px-Orlen_logo.svg.png', price: 1.19 }
+    { id: 1, name: 'neste oil', logo: 'https://mindaugas-jacionis.github.io/degalu-kainos/img/orlen.png', price: 1.02 },
+    { id: 2, name: 'neste oil', logo: 'https://mindaugas-jacionis.github.io/degalu-kainos/img/lukoil.jpg', price: 1.00 },
+    { id: 3, name: 'neste oil', logo: 'https://mindaugas-jacionis.github.io/degalu-kainos/img/emsi.png', price: 1.02 },
+    { id: 4, name: 'neste oil', logo: 'https://mindaugas-jacionis.github.io/degalu-kainos/img/statoil.png', price: 1.08 },
+    { id: 5, name: 'neste oil', logo: 'https://mindaugas-jacionis.github.io/degalu-kainos/img/neste.png', price: 1.02 },
+    { id: 6, name: 'neste oil', logo: 'https://mindaugas-jacionis.github.io/degalu-kainos/img/Balticpetr.jpg', price: 1.19 }
   ],
   dujos: [
-    { id: 1, name: 'neste oil', logo: 'https://upload.wikimedia.org/wikipedia/en/thumb/8/83/Orlen_logo.svg/640px-Orlen_logo.svg.png', price: 0.52 },
-    { id: 2, name: 'neste oil', logo: 'https://upload.wikimedia.org/wikipedia/en/thumb/8/83/Orlen_logo.svg/640px-Orlen_logo.svg.png', price: 0.62 },
-    { id: 3, name: 'neste oil', logo: 'https://upload.wikimedia.org/wikipedia/en/thumb/8/83/Orlen_logo.svg/640px-Orlen_logo.svg.png', price: 0.57 },
-    { id: 4, name: 'neste oil', logo: 'https://upload.wikimedia.org/wikipedia/en/thumb/8/83/Orlen_logo.svg/640px-Orlen_logo.svg.png', price: 0.59 },
-    { id: 5, name: 'neste oil', logo: 'https://upload.wikimedia.org/wikipedia/en/thumb/8/83/Orlen_logo.svg/640px-Orlen_logo.svg.png', price: 0.61 },
-    { id: 6, name: 'neste oil', logo: 'https://upload.wikimedia.org/wikipedia/en/thumb/8/83/Orlen_logo.svg/640px-Orlen_logo.svg.png', price: 0.42 }
+    { id: 1, name: 'neste oil', logo: 'https://mindaugas-jacionis.github.io/degalu-kainos/img/orlen.png', price: 0.52 },
+    { id: 2, name: 'neste oil', logo: 'https://mindaugas-jacionis.github.io/degalu-kainos/img/lukoil.jpg', price: 0.62 },
+    { id: 3, name: 'neste oil', logo: 'https://mindaugas-jacionis.github.io/degalu-kainos/img/emsi.png', price: 0.57 },
+    { id: 4, name: 'neste oil', logo: 'https://mindaugas-jacionis.github.io/degalu-kainos/img/statoil.png', price: 0.59 },
+    { id: 5, name: 'neste oil', logo: 'https://mindaugas-jacionis.github.io/degalu-kainos/img/neste.png', price: 0.61 },
+    { id: 6, name: 'neste oil', logo: 'https://mindaugas-jacionis.github.io/degalu-kainos/img/Balticpetr.jpg', price: 0.42 }
   ]
 }
